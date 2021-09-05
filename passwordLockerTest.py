85293db14a448a77bf1cf33796221d48d779fbe9
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-05 23:02:57 +0300

    This would create a setup method that contains new user information as well as his account credentials

--- passwordLockerTest.py
+++ passwordLockerTest.py
@@ -1,1 +1,8 @@
+from passwordLoker import User,Credentials
+import unittest 
 
+class TestPassword(unittest.TestCase):
+    
+    def setUp(self):
+        self.new_user = User("Joesh Gichon","JG","5678")
+        self.new_credentials = Credentials("facebook","JG","5678")
