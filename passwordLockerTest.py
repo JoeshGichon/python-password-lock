5c61ceec4f8471dec44d41cb3970b8ab0d7d4444
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-05 23:09:26 +0300

    This would create the tear down method that checks on user list and user credential list, test init method is created which initializes our test variables

--- passwordLockerTest.py
+++ passwordLockerTest.py
@@ -2,7 +2,19 @@ from passwordLoker import User,Credentials
 import unittest 
 
 class TestPassword(unittest.TestCase):
-    
+
     def setUp(self):
         self.new_user = User("Joesh Gichon","JG","5678")
         self.new_credentials = Credentials("facebook","JG","5678")
+
+    def tearDown(self):
+        User.users_list = []
+        Credentials.credentials_list = []
+    
+    def test_init(self):
+        self.assertEqual(self.new_user.official_name,"Joesh Gichon")
+        self.assertEqual(self.new_user.username,"mn")
+        self.assertEqual(self.new_user.password,"5678")
+        self.assertEqual(self.new_credentials.account,"facebook")
+        self.assertEqual(self.new_credentials.username,"JG")
+        self.assertEqual(self.new_credentials.password,"5678")
