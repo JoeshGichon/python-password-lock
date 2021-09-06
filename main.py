a371fd761ed344a1c56873faa18535820581341e
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-06 11:15:22 +0300

    Added a save user details and save credential details functions that save user details as well as his account credentials

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ def create_credentials(account,account_u_name,account_p_word):
     new_credential = Credentials(account,account_u_name,account_p_word)
     return new_credential
 
+def save_user_details(user): 
+    user.save_user()
+  
+def save_credentials_details(credentials): 
+    credentials.save_credentials()
+
 
