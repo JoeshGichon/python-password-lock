ac4cc8f462548d2fb76132653dead52df1e3c319
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-06 11:11:01 +0300

    Added a create user function as well create credential function that returns user information and account credenrials

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from passwordLoker import User,Credentials
 
+def create_user(o_name,u_name,p_word): 
+    new_user = User(o_name,u_name,p_word)
+    return new_user
+
+def create_credentials(account,account_u_name,account_p_word): 
+    new_credential = Credentials(account,account_u_name,account_p_word)
+    return new_credential
 
 
